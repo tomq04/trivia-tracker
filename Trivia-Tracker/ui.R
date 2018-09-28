2344afa39089cba19eaafcc87ef1872450aee2fd
tomq04 <43661860+tomq04@users.noreply.github.com>
2018-09-28 14:43:55 -0700

    Uploaded
    asdf

--- Trivia-Tracker/ui.R
+++ Trivia-Tracker/ui.R
@@ -9,15 +9,43 @@
 
 library(shiny)
 
-## Only run examples in interactive R sessions
-if (interactive()) {
-  
-  ui <- fluidPage(
-    textInput("caption", "Caption", "Data Summary"),
-    verbatimTextOutput("value")
-  )
-  server <- function(input, output) {
-    output$value <- renderText({ input$caption })
+
+# Define the fields we want to save from the form
+fields <- "name"
+outputDir <- "Responses"
+
+# Shiny app with 3 fields that the user can submit data for
+shinyApp(
+  ui = fluidPage(
+    DT::dataTableOutput("responses", width = 300), tags$hr(),
+    textInput("name", "Name", ""),
+    actionButton("submit", "submit")
+  ),
+  server = function(input, output, session) {
+    
+    # Whenever a field is filled, aggregate all form data
+    formData <- reactive({
+      data <- sapply(fields, function(x) input[[x]])
+      data
+    })
+    
+    # When the Submit button is clicked, save the form data
+    observeEvent(input$submit, {
+      save_data_flatfile <- function(responses) {
+        data <- t(responses)
+        file_name <- sprintf("%s_%s.csv", as.integer(Sys.time()), digest::digest(data))
+    # Write the file to the local system
+        write.csv(responses, file = file.path(outputDir, file_name), 
+                  row.names = FALSE, quote = TRUE)
+      }
+      save_data_flatfile(formData())
+    })
+    
+    # Show the previous responses
+    # (update with current response when Submit is clicked)
+    # output$responses <- DT::renderDataTable({
+    # input$submit
+    #  loadData()
+         
   }
-  shinyApp(ui, server)
-}
+)
